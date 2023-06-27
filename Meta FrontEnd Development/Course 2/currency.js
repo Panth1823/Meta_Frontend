79906adfb261cea181961ad5b5db6144563119ae
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-06-27 19:53:55 +0530

    course 2 code added

---
+++ Meta FrontEnd Development/Course 2/currency.js
@@ -0,0 +1,10 @@
+let INR = 100000;
+let USD = console.log("Current Rate 1 USD = 82 INR");
+const exchangeRate= 0.012;
+
+function covertCurr(INR,exchangeRate){
+    return INR * exchangeRate;
+}
+
+USD = covertCurr(  INR,exchangeRate );
+console.log(INR,"INR","=",USD,"USD");
