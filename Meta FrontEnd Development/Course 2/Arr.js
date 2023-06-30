56cf0f79d853f6de4f6ed5190988946827f84ef3
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-06-30 22:51:58 +0530

    Delete Meta FrontEnd Development/Course 2 directory

--- Meta FrontEnd Development/Course 2/Arr.js
+++
@@ -1,8 +0,0 @@
-let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"];
-function fruitArr(fruits){
-    for (let i=0;i<fruits.length;i++){
-        console.log(i,fruits[i]);
-    }
-
-}
-fruitArr(fruits);
--- Meta FrontEnd Development/Course 2/Math Random.js
+++
@@ -1,3 +0,0 @@
-let got=Math.random();
-let goti = Math.floor(got);
-console.log(goti)
