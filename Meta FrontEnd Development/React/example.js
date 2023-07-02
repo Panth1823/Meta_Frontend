fa8194fa1e21d662725d125772c1b2c3648c3018
Panth Shah <89696311+Panth1823@users.noreply.github.com>
2023-07-02 23:57:19 +0530

    Add files via upload

---
+++ Meta FrontEnd Development/React/example.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  function addTodo() {
+    setTodos([...todos, inputValue]);
+    setInputValue('');
+  }
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
